241d9d97d5b9d46c671e2147ad7c9f4742c5fabc
Wilson Padilla <48686887+wdavid73@users.noreply.github.com>
2025-06-19 14:53:53 -0500

    Initial commit

---
+++ lib/app/dependency_injection.dart
@@ -0,0 +1,77 @@
+import 'package:flutter_starter_kit/api/api.dart';
+import 'package:flutter_starter_kit/data/data.dart';
+import 'package:flutter_starter_kit/domain/repositories/repositories.dart';
+import 'package:flutter_starter_kit/domain/usecases/usecases.dart';
+import 'package:flutter_starter_kit/ui/blocs/blocs.dart';
+import 'package:flutter_starter_kit/ui/cubits/cubits.dart';
+import 'package:flutter_starter_kit/ui/shared/service/service.dart';
+import 'package:get_it/get_it.dart';
+
+final GetIt getIt = GetIt.instance;
+
+class AppDependencyInjection {
+  static void init() {
+    /// Repositories
+    getIt.registerLazySingleton<AuthRepository>(
+      () => AuthRepositoryImpl(
+        ApiAuthDataSource(ApiClient.instance),
+      ),
+    );
+
+    getIt.registerLazySingleton<ProductsRepository>(
+      () => ProductsRepositoryImpl(
+        ApiProductDataSource(ApiClient.instance),
+      ),
+    );
+
+    /// UseCases
+    getIt.registerLazySingleton<AuthUseCase>(
+      () => AuthUseCase(getIt<AuthRepository>()),
+    );
+
+    getIt.registerLazySingleton<ProductsUseCase>(
+      () => ProductsUseCase(getIt<ProductsRepository>()),
+    );
+
+    /// Services
+    getIt.registerLazySingleton<KeyValueStorageService>(
+      () => KeyValueStorageServiceImpl(),
+    );
+
+    ///  Cubits and BLoCs
+
+    // Singleton
+    getIt.registerLazySingleton<ThemeModeCubit>(() => ThemeModeCubit());
+
+    getIt.registerLazySingleton<IntroductionCubit>(
+      () => IntroductionCubit(getIt.get<KeyValueStorageService>()),
+    );
+
+    getIt.registerLazySingleton<AuthBloc>(
+      () => AuthBloc(
+        getIt<AuthUseCase>(),
+        getIt<KeyValueStorageService>(),
+      ),
+    );
+
+    getIt.registerLazySingleton<ProductsBloc>(
+      () => ProductsBloc(
+        getIt.get<ProductsUseCase>(),
+      ),
+    );
+
+    // Factory
+
+    getIt.registerFactory<SignInFormCubit>(
+      () => SignInFormCubit(
+        authBloc: getIt<AuthBloc>(),
+      ),
+    );
+
+    getIt.registerFactory<RegisterFormCubit>(
+      () => RegisterFormCubit(
+        authBloc: getIt<AuthBloc>(),
+      ),
+    );
+  }
+}
